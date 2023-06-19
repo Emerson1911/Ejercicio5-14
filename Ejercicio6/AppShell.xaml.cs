ca371544b4adf74b2d8f51501be147fb3380edc8
Emers <Emers@DESKTOP-RC7M2CL>
2023-06-19 15:57:38 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio6/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejercicio6;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Ejercicio6/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio6;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Calcular_Clicked(object sender, EventArgs e)
+    {
+		if (!string.IsNullOrEmpty(altura.Text) && !string.IsNullOrEmpty(bases.Text) && !string.IsNullOrEmpty(bases2.Text))
+		{
+			var alturax = Convert.ToInt32(altura.Text);
+			var basex = Convert.ToInt32(bases.Text);
+            var basesx = Convert.ToInt32(bases2.Text);
+            var resp1 = alturax + basex + alturax + basesx;
+			var resp2 = alturax * basex;
+			Perimetro.Text = Convert.ToString(resp1);
+			Superficie.Text = Convert.ToString(resp2);
+
+		}
+		else
+		{
+			DisplayAlert("Error","Escibre animal","Adoptado");
+		}
+    }
+}
+
